0041eb91924d74d49b0c56800c3a9ff77c18e2d1
alwin <geraldcheu@gmail.com>
2019-10-15 23:21:57 +0800

    added object pooler and some test objects

---
+++ OpenHouse2020/Assets/Game/Scenes/FoodTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodTest : OVRGrabbable
+{
+    private void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name.Equals("Knife"))
+        {
+            for (int i = 0; i < 2; ++i)
+            {
+                GameObject.Find("GameManager").GetComponent<ObjectPooler>().SpawnFromPool(this.gameObject.name, new Vector3(this.transform.position.x + i, this.transform.position.y, this.transform.position.z + i), this.transform.rotation, new Vector3(this.transform.localScale.x * 0.5f, this.transform.localScale.y * 0.5f, this.transform.localScale.z * 0.5f));
+            }
+        }
+    }
+}
