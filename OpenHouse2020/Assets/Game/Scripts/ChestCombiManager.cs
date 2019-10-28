0023519e6bfc4b287f4b7d73e58b77a5a3df6134
alwin <geraldcheu@gmail.com>
2019-10-28 19:02:22 +0800

    works but not tested with controller

--- OpenHouse2020/Assets/Game/Scripts/ChestCombiManager.cs
+++ OpenHouse2020/Assets/Game/Scripts/ChestCombiManager.cs
@@ -9,15 +9,26 @@ public class ChestCombiManager : MonoBehaviour
 
     int[,] arr_chestCombi;
 
+    public List<int> arr_testingCombi;
+
     // Start is called before the first frame update
     void Start()
     {
-        arr_chestCombi = new int[3, 4]{{5, 6, 5, 3 }, {2, 6, 3, 6 }, {1, 7, 4, 8}};
+        arr_chestCombi = new int[3, 4] { { 5, 6, 5, 3 }, { 2, 6, 3, 6 }, { 1, 7, 4, 8 } };
+        arr_testingCombi = new List<int>(4);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
+
+    //void OnCollisionEnter(Collision collision)
+    //{
+    //    //Check for a match with the specified name on any GameObject that collides with your GameObject
+    //    if (collision.gameObject.name == "")
+    //    {
+
+    //    }
+    //}
 }
