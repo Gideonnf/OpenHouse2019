8fd60e9d8aabde8e5a7131bc7dc6f067c9609c26
alwin <geraldcheu@gmail.com>
2019-11-19 19:30:04 +0800

    added monitor puzzle

---
+++ SIDMEscape/Assets/Game/Scripts/Puzzles/Monitor-arrange/MiseEnPlace.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiseEnPlace : MonoBehaviour
+{
+    Vector3[,] v3_placePos;
+
+    [SerializeField]
+    GameObject go_Monitor;
+    [SerializeField]
+    GameObject[] go_Objects;
+    [SerializeField]
+    GameObject[] go_Places;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        v3_placePos = new Vector3[,] { {new Vector3(1.391f, -0.156f, -2.37f), new Vector3(0.436f, -0.156f, -2.464f), new Vector3(0.782f, -0.156f, -2.623f) },
+                                                               {new Vector3(1.652f, -0.156f, -2.526f), new Vector3(0.975f, -0.156f, -2.464f), new Vector3(0.545f, -0.156f, -2.392f) },
+                                                               {new Vector3(0.545f, -0.156f, -2.358f), new Vector3(0.973f, -0.156f, -2.589f), new Vector3(1.56f, -0.156f, -2.352f) }};
+
+        for (int i = 0; i < go_Places.Length; ++i)
+        {
+            if ((int)go_Monitor.GetComponent<MonitorRandomiser>().n_monitorStates == 0)
+                break;
+
+            go_Places[i].transform.position = v3_placePos[(int)go_Monitor.GetComponent<MonitorRandomiser>().n_monitorStates - 1, i];
+        }
+
+        for (int i = 0; i < go_Objects.Length; ++i)
+        {
+            go_Objects[i].transform.position = new Vector3(Random.Range(0.24f, 1.8f), go_Objects[i].transform.position.y, Random.Range(-2.707f, -2.3f) + 2);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ SIDMEscape/Assets/Game/Scripts/Puzzles/Monitor-arrange/MonitorObject.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonitorObject : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.StartsWith(this.gameObject.tag) && other.gameObject.tag.Contains("place"))
+        {
+            this.gameObject.GetComponent<OVRGrabbable>().GrabEnd(Vector3.zero, Vector3.zero);
+            this.transform.position = new Vector3(other.transform.position.x, this.transform.position.y, other.transform.position.z);
+        }
+    }
+}
