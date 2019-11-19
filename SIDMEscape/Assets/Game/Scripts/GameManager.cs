91e209f9b979c22d2f749ce84244fc8d48b55104
alwin <geraldcheu@gmail.com>
2019-11-19 16:46:17 +0800

    added blitz mode

---
+++ SIDMEscape/Assets/Game/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    #region Singleton
+    private static GameManager instance = new GameManager();
+
+    private GameManager() { }
+
+    public static GameManager GetInstance()
+    {
+        return instance;
+    }
+    #endregion
+
+    public bool b_blitzMode { get; set; }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
