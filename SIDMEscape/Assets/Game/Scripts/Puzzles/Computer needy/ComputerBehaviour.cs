6b4b1e6f5aa5a57637b07a435167dee984a95f08
alwin <geraldcheu@gmail.com>
2019-12-20 17:06:54 +0800

    needy works

--- SIDMEscape/Assets/Game/Scripts/Puzzles/Computer needy/ComputerBehaviour.cs
+++ SIDMEscape/Assets/Game/Scripts/Puzzles/Computer needy/ComputerBehaviour.cs
@@ -6,8 +6,12 @@ public class ComputerBehaviour : MonoBehaviour
 {
     float timeTillFire = 0.0f;
 
+    [SerializeField]
     ParticleSystem ps_fire;
+    [SerializeField]
     ParticleSystem ps_smoke;
+
+    bool needyEnabled = true;
 
     // Start is called before the first frame update
     void Start()
@@ -21,8 +25,10 @@ public class ComputerBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (timeTillFire == 0.0f)
+        if (timeTillFire <= 0.0f && needyEnabled)
         {
+            needyEnabled = false;
+
             //stop all particles
             ps_fire.gameObject.SetActive(false);
             ps_smoke.gameObject.SetActive(false);
@@ -37,7 +43,9 @@ public class ComputerBehaviour : MonoBehaviour
             //timeTillFire = Random.Range(5, 50);
         }
         else
+        {
             timeTillFire -= 1 * Time.deltaTime;
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -50,6 +58,8 @@ public class ComputerBehaviour : MonoBehaviour
             //stop all particles
             ps_fire.gameObject.SetActive(false);
             ps_smoke.gameObject.SetActive(false);
+
+            needyEnabled = true;
         }
     }
 }
