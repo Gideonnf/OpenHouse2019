c79d7b2d27d919b89925d568bb5cad7ce4483f70
gideon <gideonnfrancis999@gmail.com>
2019-12-21 03:20:52 +0800

    Created tool tip manager havent tested

--- SIDMEscape/Assets/Game/Scripts/VRScripts/OVRTooltip.cs
+++ SIDMEscape/Assets/Game/Scripts/VRScripts/OVRTooltip.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 /// <summary>
 /// For displaying a world space canvas/UI Canvas of information
+/// Place this on objects that you want to have tooltip for
 /// </summary>
 public class OVRTooltip : MonoBehaviour
 {
+    [Tooltip("Reference to the main VR Camera")]
+    VRPlayerManager OVRPlayerReference;
+    [Header("Tooltip Configuration")]
+    [Tooltip("World space Canvas tool tip prefab")]
+    public GameObject tooltipCanvas;
+    [Tooltip("Tooltip Text to be displayed")]
+    [TextArea(15, 20)]
+    public string toolTip = "";
 
-
+    private GameObject localTooltipReference;
     // Start is called before the first frame update
     void Start()
     {
-        
+        OVRPlayerReference = VRPlayerManager.Instance;
     }
 
     // Update is called once per frame
@@ -21,12 +32,54 @@ public class OVRTooltip : MonoBehaviour
         
     }
 
+    protected bool CreateCanvasTooltip()
+    {
+        if (OVRPlayerReference == null)
+        {
+            return false;
+        }
+
+        GameObject toolTipCanvas = GameObject.Instantiate(tooltipCanvas);
+        toolTipCanvas.GetComponentInChildren<TextMeshProUGUI>().text = toolTip;
+
+        // Try to create it and store in the player reference
+        if(OVRPlayerReference.CreateWorldTooltipCanvas(toolTipCanvas))
+        {
+            return true;
+        }
+        else
+        {
+            Destroy(toolTipCanvas);
+            return false;
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         // Check if it is a hand
         if (other.gameObject.tag == "Hands")
         {
+            if(CreateCanvasTooltip())
+            {
+                localTooltipReference = OVRPlayerReference.GetWorldTipCanvas();
+            }
+        }
+    }
 
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Hands")
+        {
+            // If its null means that it isnt created yet because theres already an existing tool tip
+            if (localTooltipReference == null && OVRPlayerReference.GetWorldTipCanvas() == null)
+            {
+                // Try to create it
+                if (CreateCanvasTooltip())
+                {
+                    // Store it
+                    localTooltipReference = OVRPlayerReference.GetWorldTipCanvas();
+                }
+            }
         }
     }
 
@@ -35,7 +88,21 @@ public class OVRTooltip : MonoBehaviour
         // Check if it is a hand
         if (other.gameObject.tag == "Hands")
         {
+            // if there isnt even any lcoal tool tip then it means that it belongs to another tool tip object
+            if (localTooltipReference == null)
+                return;
 
+            // Check if it is the same first
+            // Just for safety
+            if (localTooltipReference == OVRPlayerReference.GetWorldTipCanvas())
+            {
+                // Destroy it
+                if (OVRPlayerReference.DeleteWorldTipCanvas())
+                {
+                    // Set this back to null
+                    localTooltipReference = null;
+                }
+            }
         }
     }
 }
---
+++ SIDMEscape/Assets/Game/Scripts/VRScripts/VRPlayerManager.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRPlayerManager : MonoBehaviour
+{
+    private static VRPlayerManager _instance;
+    public static VRPlayerManager Instance { get { return _instance; } }
+
+    /// <summary>
+    /// This variable holds a reference to any active tool tip canvas
+    /// This is to prevent overlap of canvas
+    /// </summary>
+    [System.NonSerialized]
+    public GameObject toolTipCanvasReference = null;
+
+    [Tooltip("Main Camera Reference")]
+    public GameObject mainCameraReference;
+
+    [Tooltip("Offset for tooltip canvas")]
+    public float toolTipOffset = 5.0f;
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        else if (_instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(toolTipCanvasReference != null)
+        {
+            Vector3 TargetLocation = mainCameraReference.transform.position + (mainCameraReference.transform.forward * toolTipOffset);
+            toolTipCanvasReference.transform.position = Vector3.Slerp(toolTipCanvasReference.transform.position, TargetLocation, Time.deltaTime);
+        }
+    }
+
+    /// <summary>
+    /// Get the reference to the main camera
+    /// </summary>
+    /// <returns></returns>
+    public GameObject GetCameraReference()
+    {
+        return mainCameraReference;
+    }
+
+    /// <summary>
+    /// Gets the tool tip canvas preference from the player manager
+    /// </summary>
+    /// <returns></returns>
+    public GameObject GetWorldTipCanvas()
+    {
+        return toolTipCanvasReference;
+    }
+
+    /// <summary>
+    /// Deletes the tool tip canvas reference so that another one can be made later on
+    /// </summary>
+    /// <returns></returns>
+    public bool DeleteWorldTipCanvas()
+    {
+        // Nothing to delete
+        if (toolTipCanvasReference == null)
+            return false;
+
+        Destroy(toolTipCanvasReference);
+        toolTipCanvasReference = null;
+
+        return true;
+    }
+
+    /// <summary>
+    /// Stores the world space canvas for tool tips created
+    /// </summary>
+    /// <param name="toolTipCanvas"></param>
+    /// <returns></returns>
+    public bool CreateWorldTooltipCanvas(GameObject toolTipCanvas)
+    {
+        // If there is already a canvas present
+        if (toolTipCanvasReference != null)
+            return false;
+
+        // Store the tooltipCanvasReference to the player class
+        toolTipCanvasReference = toolTipCanvas;
+
+        return true;
+    }
+
+}
