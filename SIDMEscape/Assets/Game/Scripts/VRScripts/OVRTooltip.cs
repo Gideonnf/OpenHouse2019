79ca47fa37ef6f30018c1a399faccddd1a9dae7a
gideon <gideonnfrancis999@gmail.com>
2019-12-21 00:38:22 +0800

    imported text mesh pro and moved a few files

---
+++ SIDMEscape/Assets/Game/Scripts/VRScripts/OVRTooltip.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// For displaying a world space canvas/UI Canvas of information
+/// </summary>
+public class OVRTooltip : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if it is a hand
+        if (other.gameObject.tag == "Hands")
+        {
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // Check if it is a hand
+        if (other.gameObject.tag == "Hands")
+        {
+
+        }
+    }
+}
