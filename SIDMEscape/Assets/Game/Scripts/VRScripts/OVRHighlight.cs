4f4579bae28ec3bdcd9d6ecfe5243fcafcbccfe2
gideon <gideonnfrancis999@gmail.com>
2019-12-20 12:25:55 +0800

    creating scripts for later use and moving scripts around

---
+++ SIDMEscape/Assets/Game/Scripts/VRScripts/OVRHighlight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This highlights objects that are near the hand
+/// </summary>
+public class OVRHighlight : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
