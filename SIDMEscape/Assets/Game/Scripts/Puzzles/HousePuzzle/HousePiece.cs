e1d2f373ce4312e5e6d7dc88216a444018471d60
alwin <geraldcheu@gmail.com>
2020-01-07 15:19:17 +0800

    House puzzle

--- SIDMEscape/Assets/Game/Scripts/Puzzles/HousePuzzle/HousePiece.cs
+++ SIDMEscape/Assets/Game/Scripts/Puzzles/HousePuzzle/HousePiece.cs
@@ -13,6 +13,15 @@ public class HousePiece : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if ((this.transform.localPosition.x < 1 && this.transform.localPosition.y < 1 && this.transform.localPosition.z < 1) &&
+            (this.transform.localPosition.x > -1 && this.transform.localPosition.y > -1 && this.transform.localPosition.z > -1))
+        {
+            if ((this.transform.localEulerAngles.x < 5 && this.transform.localPosition.y < 5 && this.transform.localPosition.z < 5) &&
+            (this.transform.localPosition.x > 355 && this.transform.localPosition.y > 355 && this.transform.localPosition.z > 355))
+            {
+                this.transform.localPosition = Vector3.zero;
+                this.transform.localEulerAngles = Vector3.zero;
+            }
+        }
     }
 }
--- SIDMEscape/Assets/Game/Scripts/Puzzles/HousePuzzle/HousePuzzle.cs
+++ SIDMEscape/Assets/Game/Scripts/Puzzles/HousePuzzle/HousePuzzle.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class HousePuzzle : MonoBehaviour
 {
+    List<GameObject> goArr_HousePieces;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        goArr_HousePieces = new List<GameObject>();
+
+        for (int i = 0; i < this.transform.childCount; ++i)
+        {
+            if (this.transform.GetChild(i).gameObject.GetComponent<HousePiece>() == null)
+                continue;
+
+            goArr_HousePieces.Add(this.transform.GetChild(i).gameObject);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        foreach (var go in goArr_HousePieces)
+        {
+            if (go.transform.localPosition != Vector3.zero)
+                break;
+
+            PuzzleLightManager.GetInstance().nextLight();
+        }
     }
 }
