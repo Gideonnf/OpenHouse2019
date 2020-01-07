85b525ccfc3dd12839c3a5a93aa3ae256111ec78
alwin <geraldcheu@gmail.com>
2020-01-07 22:50:27 +0800

    updated monitor randomiser

--- SIDMEscape/Assets/Game/Scripts/Puzzles/Monitor-arrange/MonitorRandomiser.cs
+++ SIDMEscape/Assets/Game/Scripts/Puzzles/Monitor-arrange/MonitorRandomiser.cs
@@ -7,12 +7,11 @@ public class MonitorRandomiser : MonoBehaviour
     System.Random rnd = new System.Random(); //init randomiser C# style
 
     [Header("Monitor Settings")]
-    [Tooltip("Store the materials to change the monitor color ")]
-    [SerializeField]
-    Material[] arr_monitorMaterials;
-
-    [SerializeField]
-    Material _default;
+    //[Tooltip("Store the materials to change the monitor color ")]
+    //[SerializeField]
+    //Material[] arr_monitorMaterials;
+    //[SerializeField]
+    //Material _default;
 
     GameObject go_Monitor;
 
@@ -28,16 +27,16 @@ public class MonitorRandomiser : MonoBehaviour
     };
     public monitor_states n_monitorStates;
 
-    int[,] arr_monitorColourCombi;
-    int[,] arr_monitorColourBlitzCombi;
+    //int[,] arr_monitorColourCombi;
+    //int[,] arr_monitorColourBlitzCombi;
 
-    float f_ctimer = 0;
-    int n_citer = 0;
+    //float f_ctimer = 0;
+    //int n_citer = 0;
 
     // Start is called before the first frame update
     void Awake()
     {
-        arr_monitorColourCombi = new int[,] { { 0, 1, 2, 1}, { 1, 2, 0, 3}, {2, 0, 2, 1 } }; //colour combinations, each int is the array position of arr_monitorMaterials
+        //arr_monitorColourCombi = new int[,] { { 0, 1, 2, 1}, { 1, 2, 0, 3}, {2, 0, 2, 1 } }; //colour combinations, each int is the array position of arr_monitorMaterials
 
         go_Monitor = this.gameObject;
 
@@ -47,24 +46,25 @@ public class MonitorRandomiser : MonoBehaviour
     // Update is called once per frame
     void Update()
     { 
-        f_ctimer += Time.deltaTime * 1; //timer for per second to change the colour
+        //commented out cause this does the colour changing
+        //f_ctimer += Time.deltaTime * 1; //timer for per second to change the colour
 
-        //cycling through colours
-        if (f_ctimer > 1)
-        {
-            if (n_citer >= 5)
-                n_citer = 0;
+        ////cycling through colours
+        //if (f_ctimer > 1)
+        //{
+        //    if (n_citer >= 5)
+        //        n_citer = 0;
 
-            if (n_citer == 0)
-            {
-                go_Monitor.GetComponent<Renderer>().materials = new Material[1] { _default };
-            }
-            else //get the monitor material from the colour combi array
-                go_Monitor.GetComponent<Renderer>().materials = new Material[1] { arr_monitorMaterials[arr_monitorColourCombi.GetRow((int)n_monitorStates)[n_citer - 1]] }; //n_citer - 1 cause 0 wil be default
+        //    if (n_citer == 0)
+        //    {
+        //        go_Monitor.GetComponent<Renderer>().materials = new Material[1] { _default };
+        //    }
+        //    else //get the monitor material from the colour combi array
+        //        go_Monitor.GetComponent<Renderer>().materials = new Material[1] { arr_monitorMaterials[arr_monitorColourCombi.GetRow((int)n_monitorStates)[n_citer - 1]] }; //n_citer - 1 cause 0 wil be default
 
-            f_ctimer = 0;
-            ++n_citer;
-        }
+        //    f_ctimer = 0;
+        //    ++n_citer;
+        //}
 
         //checking if correct coaster
         for (int i = 0; i < go_TablePuzzle.transform.childCount - 1; ++i)
