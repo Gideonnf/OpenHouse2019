e108b361de94f57679ca9bb2748f66751401a534
gideon <gideonnfrancis999@gmail.com>
2020-01-09 05:37:03 +0800

    Sounds

--- SIDMEscape/Assets/Game/Scripts/SoundManager.cs
+++ SIDMEscape/Assets/Game/Scripts/SoundManager.cs
@@ -2,8 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class AudioObject
+{
+    public string name;
+    public AudioClip audioFile;
+    public bool isLooping = false;
+}
+
 public class SoundManager : MonoBehaviour
 {
+    public static SoundManager instance = null;
+    
+    public List<AudioObject> ListOfAudioObjects = new List<AudioObject>();
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+    }
     // Start is called before the first frame update
     void Start()
     {
